b99c34f9c284d2417f544be4e9566a49c99b2427
emon21 <emonbd66@gmail.com>
2023-05-03 10:17:21 +0600

    feat:added calltoaction section design complate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
+    colors: {
+      primary: '#008AFF',
+      secondary: '#222D39',
+      gray: '#8492A7',
+      white: '#ffffff',
+    },
     fontFamily: {
-        custom: ['Hanken Grotesk', 'sans-serif'],
-      },
-    extend: {
-        colors:{
-            btnprimary:"#008AFF",  
-            },
+      custom: ['Hanken Grotesk', 'sans-serif'],
+    },
 
-        },
-        
-  
     container: {
-        center: true,
-        padding: "0rem",
-        screens: {
-            sm: "600px",
-            md: "728px",
-            lg: "920px",
-            xl: "1024px",
-            "2xl": "1320px",
-        },
+      center: true,
+      padding: '0rem',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '920px',
+        xl: '1024px',
+        '2xl': '1320px',
+      },
     },
   },
   plugins: [],
-}
+};
